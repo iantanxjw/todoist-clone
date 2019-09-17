ef22f86dd12613207e26f647edd83c146c71a9ef
meimein <723306@AMBAU00480.local>
2019-09-17 11:45:08 +1000

    ordered lists added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Header } from './components/layout/Header';
+import { Content } from './components/layout/Content';
+import { Sidebar } from './components/layout/Sidebar';
 
 export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello there!</p>
+        <Header />
+        <Content />
       </header>
     </div>
   );
